fcd3c51548a0033101738442b613ecff429d02b4
connormcd22 <connor.mcdonough28@gmail.com>
2018-08-30 17:57:17 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,7 +21,9 @@ var cards=[
   },
 ];
 var cardsInPlay=[];
-var checkForMatch=function(){
+var checkForMatch=function(cardId){
+
+  this.setAttribute('src',(cards[cardId].cardImage));
   if(cardsInPlay.length===2){
     if(cardsInPlay[0]===cardsInPlay[2]){
       alert('you found a mathch');
@@ -31,12 +33,23 @@ var checkForMatch=function(){
     }
   }
 }
-var flipCard=function(cardId){
+var flipCard=function(){
+  var cardId=this.getAttribute('data-id');
+   checkForMatch.bind(this)(cardId);
   console.log("User flipped "+ cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
-  checkForMatch();
 }
-flipCard(0);
-flipCard(2);
+var createBoard=function() {
+for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+  cardElement.setAttribute('src', "images/back.png");
+  cardElement.setAttribute('data-id',i);
+
+  cardElement.addEventListener('click', flipCard);
+  document.getElementById('game-board').appendChild(cardElement);
+
+}
+}
+createBoard();
